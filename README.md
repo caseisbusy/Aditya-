64d192a01a559fecaf9b7b36c870cef19bd59397
Pwetty Pwincess <86605082+CatgirlsSimp@users.noreply.github.com>
2022-03-25 10:45:36 +0000

    updated trackStart.ts a bit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run
+
+## First write in terminal `java -jar Lavalik.jar` and click enter
+## Second write in terminal `npm start` to run your code
+## Last step is to wait for the bot to run and for the commands and slash commands to register
--- events/Lavalink/trackStart.ts
+++ events/Lavalink/trackStart.ts
@@ -39,5 +39,43 @@ client.manager.on("trackStart", async(player, track, payload) => {
 		components: [row, row2]
 	})
 	player.setNowplayingMessage(playing)
-	
+
+	const embed2 = new MessageEmbed()
+	.setTimestamp()
+	const colletor = playing.createMessageComponentCollector({
+		filter: (filt) => {
+			if(filt.guild.me.voice.channel && filt.guild.me.voice.channelId === filt.member.voice.channel) { 
+			return true;
+		} else {
+				filt.reply({ content: "You must be in a voice channel", ephemeral: true }) return false
+		}
+	}
+	time: track.duration,
+	})
+	collector.on("collect" async interaction => {
+			await interaction.deferReply({
+				ephemeral: true
+			});
+		if(interaction.customId === "lowervolume") {
+			if(!player) return interaction.editReply({ content: "There is no song playing" })
+			let volume = Number(player.volume) - 10
+			await player.setVolume(volume)
+			const e = new MessageEmbed()
+			.setAuthor({ name: interaction.member.user.tag, iconURL: interaction.member.user.displayAvatarURL({ dynamic: true })})
+			.setDescription(`🔉 Volume now set to: **${volume}**`)
+			interaction.editReply({ embeds: [e] })
+		} else if(interaction.customId === "highervolume") {
+			if(!player) return interaction.editReply({ content: "There is no song playing" })
+			  let amount = Number(player.volume) + 10;
+				const er = new MessageEmbed()
+			.setAuthor({ name: interaction.member.user.tag, iconURL: interaction.member.user.displayAvatarURL({ dynamic: true }) })
+			.setDescription(`Volume cannot be higher than 150`)
+			if (amount >= 150) return interaction.editReply({ embeds: [er] })
+      await player.setVolume(amount)
+			const e = new MessageEmbed()
+			.setAuthor({ name: interaction.member.user.tag, iconURL: interaction.member.user.displayAvatarURL({ dynamic: true })})
+			.setDescription(`🔉 Volume now set to: **${volume}**`)
+			interaction.editReply({ embeds: [e] })
+		}
+	})
 })
